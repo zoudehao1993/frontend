95e1425f7f7b117ab86d9ad8bb270b17725a50e4
dehao <15941172185@163.com>
2018-11-01 09:42:28 +0800

    加入构造函数

--- Add Two Numbers /add_two_numbers.js
+++ Add Two Numbers /add_two_numbers.js
@@ -1,4 +1,11 @@
 /**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
